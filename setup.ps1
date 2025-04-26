c9f78a7555664e086353eb52cf8952d42d7389a8
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-04-27 02:51:56 +0300

    Feat: Enhance setup script with additional module imports and git identity configuration

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,13 @@
 #Requires -Version 7
 
-@('Utils/Utils.psm1', 'Appinstaller/installer.psm1') | ForEach-Object { Import-Module (Join-Path $PSScriptRoot "modules/$_") -Force }
+@(
+  'Utils/Utils.psm1',
+  'Appinstaller/installer.psm1',
+  'Office/installer.psm1',
+  'Terminal/setup.psm1'
+) | ForEach-Object {
+  Import-Module (Join-Path $PSScriptRoot "modules/$_") -Force
+}
 
 if (-not (Test-IsAdmin)) {
   Write-ErrorStyled "This script requires administrative privileges. Please run as administrator."
@@ -21,3 +28,43 @@ if (-not(Get-Command choco -ErrorAction SilentlyContinue)) {
 }
 Write-TitleBox "Installing Apps" -Color Cyan
 Invoke-AppInstallers
+
+
+write-TitleBox "Installing Terminal tweaks" -Color Cyan
+Set-DefaultPowerShellProfile
+Install-ModulesFromJson
+Import-ModulesFromJson
+Install-CatppuccinTheme
+
+write-TitleBox "Applying dotfiles" -Color Cyan
+Copy-DotfilesFromConfig
+
+write-TitleBox "Setting up git identity" -Color Cyan
+function Set-GitIdentity {
+
+  do {
+    $gitName = Read-ColoredInput -Prompt "👤 Enter your Git username: " -Color "Magenta"
+
+
+    if ([string]::IsNullOrWhiteSpace($gitName)) {
+      Write-WarningStyled "Username cannot be empty. Please try again."
+    }
+  } while ([string]::IsNullOrWhiteSpace($gitName))
+
+  do {
+    $gitEmail = Read-ColoredInput -Prompt "📧 Enter your Git email: " -Color "Cyan"
+    if (-not (Test-IsValidEmail -Email $gitEmail)) {
+      Write-WarningStyled "Invalid email format. Please try again."
+    }
+  } while (-not (Test-IsValidEmail -Email $gitEmail))
+
+  git config --global user.name "$gitName"
+  git config --global user.email "$gitEmail"
+  Write-Host ""
+  Write-Info "Git username set to: $gitName"
+  Write-Info "Git email set to: $gitEmail"
+}
+Set-GitIdentity
+
+write-TitleBox "Installing Office" -Color Cyan
+Install-Office
