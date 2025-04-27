4e561367b331ade23d0a773fe5fdefab1cc69f65
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-04-27 13:24:19 +0300

    Feat: Add Touch function to create files and set timestamps, and enhance alias functions

--- dotfiles/powershell/aliases.ps1
+++ dotfiles/powershell/aliases.ps1
@@ -14,8 +14,57 @@ if (Get-Command lsd -ErrorAction SilentlyContinue) {
   function lla {
     lsd -la
   }
+  function lt {
+    lsd --tree --depth=2
+  }
 
 }
+# touch command
+function Touch {
+  [CmdletBinding()]
+  param(
+    [Parameter(Position = 0, Mandatory = $true, ValueFromRemainingArguments = $true)]
+    [string[]]$Path,
+
+    [Parameter()]
+    [datetime]$Time = (Get-Date),
+
+    [switch]$CreateDirectories
+  )
+
+  foreach ($p in $Path) {
+    $fullPath = Resolve-Path -Path $p -ErrorAction SilentlyContinue
+
+    if (-not $fullPath) {
+      # If the path doesn't exist
+      $directory = Split-Path -Parent $p
+
+      if ($directory -and !(Test-Path $directory)) {
+        if ($CreateDirectories) {
+          New-Item -ItemType Directory -Path $directory -Force | Out-Null
+        }
+        else {
+          Write-Error "Directory '$directory' does not exist. Use -CreateDirectories to auto-create it."
+          continue
+        }
+      }
+
+      # Create an empty file
+      New-Item -ItemType File -Path $p -Force | Out-Null
+      $item = Get-Item -LiteralPath $p
+    }
+    else {
+      $item = Get-Item -LiteralPath $p
+    }
+
+    # Set the LastWriteTime
+    $item.LastWriteTime = $Time
+    $item.LastAccessTime = $Time
+  }
+}
+
+
+
 if (Get-Command bat -ErrorAction SilentlyContinue) {
   Set-Alias cat bat -Force
 }
