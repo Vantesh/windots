1003e256c43a5e38f151855c224694c687a632fe
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-04-30 01:13:22 +0300

    Feat: Update PowerShell profile with Catppuccin color adjustments and add FZF options

--- dotfiles/powershell/profile.ps1
+++ dotfiles/powershell/profile.ps1
@@ -1,3 +1,5 @@
+$env:XDG_CONFIG_HOME = "$env:USERPROFILE\.config"
+
 # ─── Oh-My-Posh Init ──────────────────────────────────────────────
 oh-my-posh init pwsh --config "$env:USERPROFILE\.config\powershell\ohmyposh\vantesh.toml" | Invoke-Expression
 
@@ -20,7 +22,7 @@ if (Get-Module -ListAvailable -Name lsd-aliases) {
 
 # ─── Catppuccin Color Setup ──────────────────────────────────
 if ($Catppuccin) {
-  $Flavor = $Catppuccin['Mocha']
+  $Flavor = $Catppuccin['Macchiato']
 
   $Colors = @{
     ContinuationPrompt     = $Flavor.Teal.Foreground()
@@ -32,7 +34,7 @@ if ($Catppuccin) {
     ListPredictionSelected = $Flavor.Surface0.Background()
 
     Command                = $Flavor.Flamingo.Foreground()
-    Comment                = $Flavor.Overlay0.Foreground()
+    Comment                = $Flavor.Overlay1.Foreground()
     Default                = $Flavor.Text.Foreground()
     Error                  = $Flavor.Red.Foreground()
     Keyword                = $Flavor.Mauve.Foreground()
@@ -64,11 +66,43 @@ $lscolorsPath = "$env:USERPROFILE\.config\lscolors\lscolors"
 if (Test-Path $lscolorsPath) {
   $env:LS_COLORS = (Get-Content $lscolorsPath -Raw).Trim()
 }
+
 # ─── Aliases ──────────────────────────────────────────────
 $aliasFile = "$env:USERPROFILE\.config\powershell\aliases.ps1"
 if (Test-Path $aliasFile) {
   . $aliasFile
 }
+if (Get-Command fzf -ErrorAction SilentlyContinue) {
+  $ENV:FZF_DEFAULT_OPTS = @"
+ --color=fg:#cdd6f4,fg+:#d0d0d0,bg:#1e1e2e,bg+:#262626
+  --color=hl:#f38ba8,hl+:#5fd7ff,info:#cba6f7,marker:#b4befe
+  --color=prompt:#cba6f7,spinner:#f5e0dc,pointer:#f5e0dc,header:#f38ba8
+  --color=gutter:#1e1e2e,border:#313244,label:#cdd6f4,query:#d9d9d9
+  --border="rounded" --border-label="" --preview-window="border-rounded"
+  --marker=" " --pointer="◆" --separator="" --scrollbar=""
+  --layout="reverse"
+  --height=40%
+"@
+  function ff {
+    fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'
+  }
+
+}
+if (Get-Command bat -ErrorAction SilentlyContinue) {
+  $env:BAT_CONFIG_DIR = "$env:USERPROFILE\.config\bat"
+  $env:BAT_CONFIG_PATH = "$env:BAT_CONFIG_DIR\config.conf"
+}
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+if (Get-Command choco -ErrorAction SilentlyContinue) {
+  $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+  Import-Module "$ChocolateyProfile" -Global
+}
+
 # ─── Zoxide Init ──────────────────────────────────────────────
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
   Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
